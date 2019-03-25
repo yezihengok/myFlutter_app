8393773463c02a459c1923a84016c071cbdf32ef
yezihengok <295658413@qq.com>
2019-03-25 19:17:40 +0800

    文本、图片、CheckBox 控件 learning~

--- lib/com/test/weight/TextField_Form.dart
+++ lib/com/test/weight/TextField_Form.dart
@@ -271,3 +271,114 @@ class _FocusTestState1 extends State<FocusTest1> {
 }
 
 
+///Form:
+
+/* autovalidate：是否自动校验输入内容；当为true时，每一个子FormField内容发生变化时都会自动校验合法性，并直接显示错误信息。否则，需要通过调用FormState.validate()来手动校验。
+   onWillPop：决定Form所在的路由是否可以直接返回（如点击返回按钮），该回调返回一个Future对象，如果Future的最终结果是false，则当前路由不会返回；如果为true，则会返回到上一个路由。此属性通常用于拦截返回按钮。
+   onChanged：Form的任意一个子FormField内容发生变化时会触发此回调。*/
+
+///为了方便使用，Flutter提供了一个TextFormField,它继承自FormField类，也是TextField的一个包装类，所以除了FormField定义的属性之外，它还包括TextField的属性。
+  /*const FormField({
+  ...
+  FormFieldSetter<T> onSaved, //保存回调
+  FormFieldValidator<T>  validator, //验证回调
+  T initialValue, //初始值
+  bool autovalidate = false, //是否自动校验。
+  })*/
+
+///FormState 为Form的State类 可以通过Form.of()或GlobalKey获得。我们可以通过它来对Form的子孙FormField进行统一操作
+  /*FormState.validate()：调用此方法后，会调用Form子孙FormField的validate回调，如果有一个校验失败，则返回false，所有校验失败项都会返回用户返回的错误提示。
+    FormState.save()：调用此方法后，会调用Form子孙FormField的save回调，用于保存表单内容
+    FormState.reset()：调用此方法后，会将子孙FormField的内容清空。*/
+
+  ///一个完整的登录校验案例:
+///用户名不能为空，如果为空则提示“用户名不能为空”。
+///密码不能小于6位，如果小于6为则提示“密码不能少于6位”
+class FormTest extends StatefulWidget {
+  @override
+  _FormTestRouteState createState() => new _FormTestRouteState();
+}
+
+class _FormTestRouteState extends State<FormTest> {
+
+  TextEditingController _nameController=new TextEditingController();
+  TextEditingController _pwdController=new TextEditingController();
+  GlobalKey _formKey= new GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 24.0),
+          child: Form(
+            key: _formKey, //设置globalKey，用于后面获取FormState
+            autovalidate: true,//开启自动校验
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  autofocus: true, //获取默认焦点
+                  controller: _nameController,
+                  decoration: InputDecoration(
+                      labelText: "用户名",
+                      hintText: "用户名或邮箱",
+                      icon: Icon(Icons.person)
+                  ),
+                    // 校验用户名
+                    validator: (v) {
+                      return v.trim().length > 0 ? null : "用户名不能为空!";
+                    }
+                ),
+
+                TextFormField(
+                    controller: _pwdController,
+                    decoration: InputDecoration(
+                        labelText: "密码",
+                        hintText: "输入登录密码",
+                        icon: Icon(Icons.lock)
+                    ),
+                    obscureText: true,
+                    //校验密码
+                    validator: (v) {
+                      return v.trim().length > 5 ? null : "密码不能少于6位哦~";
+                    }
+                ),
+
+                /// 登录按钮
+                Padding(
+                  padding: const EdgeInsets.only(top: 30),
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: RaisedButton(
+                          padding: EdgeInsets.all(15.0),
+                          child: Text("登录"),
+                          color: Theme.of(context).primaryColor,
+                          textColor: Colors.white,
+                          onPressed: () {
+                            //在这里不能通过此方式获取FormState，context不对
+                            //print(Form.of(context));
+
+                            // 通过_formKey.currentState 获取FormState后，
+                            // 调用validate()方法校验用户名密码是否合法，校验
+                            // 通过后再提交数据。
+                            if((_formKey.currentState as FormState).validate()){
+
+                              print("验证通过~~");
+                            }
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+
+
+              ],
+            ),
+          ),
+        ),
+    );
+  }
+
+}
